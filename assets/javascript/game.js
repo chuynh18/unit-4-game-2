60a3da7f5aa5d55da87594d0823f00df4422dddf
Acer <38088640+chuynh18@users.noreply.github.com>
2018-04-29 18:33:53 -0700

    logic for picking your character implemented

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,21 @@
 "use strict";
 
-var playerChar;
-var gameState;
-var characters = [
+// ----------------------- variables and such -----------------------
+
+const characters = [
     {name: "Ratio Tile", src: "assets/images/ratio.jpg", class: "charImg lightSide", hp: 200, attack: 10, counter: 6},
     {name: "Space General", src: "assets/images/spacegeneral.jpg", class: "charImg darkSide", hp: 250, attack: 8, counter: 7},
     {name: "The", src: "assets/images/the.jpg", class: "charImg darkSide", hp: 150, attack: 12, counter: 11},
     {name: "Speaker D", src: "assets/images/speaker.jpg", class: "charImg darkSide", hp: 225, attack: 13, counter: 5}
 ];
+var playerChar = {};
+var activeEnemy = {};
+var waitingEnemies = [];
+
+const gameState = ["choosePlayerChar", "chooseEnemyChar", "battle"];
+var currentGameState;
+
+// ----------------------- functions -----------------------
 
 // creates the clickable characters based on data from the "characters" array of objects
 var createChars = function() {
@@ -19,13 +27,32 @@ var createChars = function() {
         charImg.attr("src", characters[i].src);
         charImg.addClass(characters[i].class);
         charImg.attr("alt", characters[i].name);
+        charImg.attr("id", i);
         $("#charSelect").append(charContainer);
         $("#char"+(i+1)).append(charImg);
     }
 };
 
-$(function() {
+var setGameState = function(gameStateIndex) {
+    currentGameState = gameStateIndex;
+}
 
+// ----------------------- game code -----------------------
+
+$(function() {
+    // on page load, put the chars on the page and set the game state to choosePlayerChar
     createChars();
+    setGameState(gameState[0]);
+
+    $(".charImg").on("click", function() {
+        if (currentGameState === gameState[0]) {
+            waitingEnemies = characters;
+            playerChar = waitingEnemies.splice(this.id,1)[0];
+            console.log("You chose: " + playerChar.name);
+            console.log(waitingEnemies);
+            setGameState(gameState[1]);
+        }
+
+    })
 
 });
